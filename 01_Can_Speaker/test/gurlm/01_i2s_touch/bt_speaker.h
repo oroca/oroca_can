538eeab4fc4c3f1e82b7de42e7d555083234be68
dlguswns2018 <dlguswns2018@daum.net>
2018-07-10 23:57:52 +0900

    add touch remote control

---
+++ 01_Can_Speaker/test/gurlm/01_i2s_touch/bt_speaker.h
@@ -0,0 +1,16 @@
+/*
+ * a2dp.h
+ *
+ *  Created on: 07.05.2017
+ *      Author: michaelboeckling
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void bt_speaker_start(void);
+
+#ifdef __cplusplus
+}
+#endif
